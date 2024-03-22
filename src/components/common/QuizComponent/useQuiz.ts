8483b2b161cef746224f22c8d38a5fece6d863e4
7xthanim2 <7xthanim2@192.168.0.100>
2024-03-23 04:00:52 +0600

    feat/001: refactor

---
+++ src/components/common/QuizComponent/useQuiz.ts
@@ -0,0 +1,137 @@
+import { useEffect, useMemo, useState } from "react";
+
+import { useAppDispatch, useAppSelector } from "hooks";
+import { getRandomElement } from "utils";
+import { fetchDictionary } from "features/dictionary/actions";
+import {
+  clearWords,
+  greenWordsAdded,
+  redWordsAdded,
+  yellowWordsAdded,
+} from "features/dictionary/slice";
+
+interface IProgressBarItem {
+  answeredCount: number;
+  progressPercent: number;
+}
+
+interface IProgressBarState {
+  green: IProgressBarItem;
+  yellow: IProgressBarItem;
+  red: IProgressBarItem;
+}
+
+export const useQuiz = () => {
+  const dispatch = useAppDispatch();
+  const { dictionary, greenWords, yellowWords, redWords } = useAppSelector(
+    (state) => state.dictionary,
+  );
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+  const [questionText, setQuestionText] = useState<string>("Переведите слово:");
+  const [userAnswer, setUserAnswer] = useState<string>("");
+  const [progressBar, setProgressBar] = useState<IProgressBarState>({
+    green: { answeredCount: 0, progressPercent: 0 },
+    yellow: { answeredCount: 0, progressPercent: 0 },
+    red: { answeredCount: 0, progressPercent: 0 },
+  });
+  const [dictionaryPercent, setDictionaryPercent] = useState<number>(100);
+
+  const unansweredWords = useMemo(() => {
+    return dictionary.filter(
+      (dictWord) =>
+        !greenWords.some(
+          (greenWord) =>
+            greenWord.ru === dictWord.ru && greenWord.en === dictWord.en,
+        ),
+    );
+  }, [dictionary, greenWords]);
+
+  const question = useMemo(() => {
+    return unansweredWords.length
+      ? `${questionText} «${unansweredWords[currentQuestionIndex].ru}»`
+      : "Вопросы кончились!";
+  }, [unansweredWords, currentQuestionIndex, questionText]);
+
+  useEffect(() => {
+    dispatch(fetchDictionary());
+  }, [dispatch]);
+
+  useEffect(() => {
+    setCurrentQuestionIndex((prev) => getRandomElement(unansweredWords, prev));
+  }, [unansweredWords]);
+
+  useEffect(() => {
+    setProgressBar({
+      green: {
+        answeredCount: greenWords.length,
+        progressPercent: (greenWords.length / dictionary.length) * 100,
+      },
+      yellow: {
+        answeredCount: yellowWords.length,
+        progressPercent: (yellowWords.length / dictionary.length) * 100,
+      },
+      red: {
+        answeredCount: redWords.length,
+        progressPercent: (redWords.length / dictionary.length) * 100,
+      },
+    });
+  }, [greenWords, yellowWords, redWords]);
+
+  useEffect(() => {
+    setDictionaryPercent((unansweredWords.length / dictionary.length) * 100);
+  }, [unansweredWords, dictionary]);
+
+  const handleClear = () => {
+    dispatch(clearWords());
+  };
+
+  const handleSubmit = () => {
+    setUserAnswer("");
+    const correctUserAnswer = userAnswer.trim().toLocaleLowerCase();
+    const dictionaryValueEn =
+      unansweredWords[currentQuestionIndex].en.toLocaleLowerCase();
+
+    let correctChars = 0;
+    let incorrectChars = Math.abs(
+      correctUserAnswer.length - dictionaryValueEn.length,
+    );
+
+    const comparisonBase =
+      correctUserAnswer.length < dictionaryValueEn.length
+        ? correctUserAnswer
+        : dictionaryValueEn;
+
+    for (let i = 0; i < comparisonBase.length; i++) {
+      if (correctUserAnswer[i] === dictionaryValueEn[i]) {
+        correctChars += 1;
+      } else incorrectChars += 1;
+    }
+
+    const errorRate = (incorrectChars / (correctChars + incorrectChars)) * 100;
+    if (!errorRate) {
+      dispatch(greenWordsAdded(unansweredWords[currentQuestionIndex]));
+      setQuestionText(`Верно - «${dictionaryValueEn}», а теперь переведите:`);
+      return;
+    }
+
+    if (errorRate < 30) {
+      dispatch(yellowWordsAdded(unansweredWords[currentQuestionIndex]));
+      setQuestionText(
+        `Почти правильно - «${dictionaryValueEn}», следующее слово:`,
+      );
+    } else {
+      dispatch(redWordsAdded(unansweredWords[currentQuestionIndex]));
+      setQuestionText(`Неверно - «${dictionaryValueEn}», идем дальше:`);
+    }
+  };
+  return {
+    dictionaryPercent,
+    unansweredWords,
+    progressBar,
+    question,
+    handleClear,
+    userAnswer,
+    setUserAnswer,
+    handleSubmit,
+  };
+};
--- src/features/dictionary/actions.ts
+++ src/features/dictionary/actions.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 import { axiosInstance } from "services/api";
-import { shuffleArray } from "utils";
 import { IDictionary } from "./types";
 
 export const fetchDictionary = createAsyncThunk(
@@ -9,7 +8,7 @@ export const fetchDictionary = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const res = await axiosInstance.get<IDictionary[]>("/dictionary.json");
-      return shuffleArray(res.data);
+      return res.data;
     } catch (e: any) {
       return thunkAPI.rejectWithValue(e.message);
     }
